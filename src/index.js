970d485d22427c2f18bde75e799300a2a5b092cc
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-06-06 20:11:17 +0100

    get wordlist from file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
 
 const { gameSettingQuestions } = require("./questions");
+
+const getCategoryFromFile = (category) => {
+	// build the file path
+	const fileName = `${category}.json`;
+
+	const filePath = path.join(__dirname, "data", fileName);
+
+	// read from file using file path
+	const rawData = fs.readFileSync(filePath, "utf8");
+
+	const parsedData = JSON.parse(rawData);
+
+	// return data from file
+	return parsedData;
+};
 
 const init = async () => {
 	// prompt the game setting questions and store answers
@@ -8,8 +25,14 @@ const init = async () => {
 
 	// log the answers
 	console.log(answers);
+
+	// ask the category question and console log the data from the chosen category
+	const categoryDataFromFile = getCategoryFromFile(answers.category);
+
+	// extract the level from categoryDataFromFile
+	const wordList = categoryDataFromFile[answers.level];
+
+	console.log(wordList);
 };
 
 init();
-
-// ask the category question and console log the data from the chosen category
